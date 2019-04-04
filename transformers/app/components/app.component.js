f8487ab4a4d78c93a66b34f10e99ccac08d68a92
Cam <cameronmcouch@gmail.com>
2019-04-04 09:23:45 -0400

    changed favico and added comments in the component

--- transformers/app/components/app.component.js
+++ transformers/app/components/app.component.js
@@ -4,14 +4,18 @@ const comp = {
     controller:['TransformerService', function(TransformerService){
         const vm = this;
         vm.clicked = "false"
+        //brings our objects over from transformers.js
         vm.bots1 = mappedTransformers1;
         vm.bots2 = mappedTransformers2;
+        //bots in the field are kept here so the data can be mutated separately from the og object
         vm.botArr = [];
+        //health for each bot in botArr, corresponds with index - this is needed to ensure we can't heal past initial health
         vm.healthArr = [];
+        //when user clicks the add button, add that transformer to the field. Limit 4 bots in the field, no duplicates.
         vm.add = function(bot) {
             for (let i = 0; i < vm.botArr.length; i++) {
                 if (bot.title === vm.botArr[i].title) {
-                    console.log("hi");
+                    alert("This Transformer is already on your team.");
                 return;
                 }
             }
@@ -20,6 +24,7 @@ const comp = {
             vm.healthArr.push(angular.copy(bot.wrench));
             }
         }
+        //when user clicks the heal button, this will add 1 to the bot health w/o going over the initial health
         vm.mathAdd = function(bot) {
             for(let i = 0; i < vm.botArr.length; i++) {
                 if(vm.botArr[i].name === bot.name) {
@@ -30,15 +35,18 @@ const comp = {
                 }
             }
         }
+        //when user clicks the damage button, this will subtract 1 from the bot health w/o going under 0
         vm.mathMin = function(bot) {
             if(bot.wrench != 0) {
             bot.wrench--;
             }
         }
+        //when field clear is clicked, clears botArr and healthArr, blanks the field, and ensures the original bot object data is not mutated
         vm.clear = function() {
             vm.botArr = [];
             vm.healthArr = [];
         }
+        //collapses/expands the menu element when user clicks the hide bar
         vm.hide = function() {
             vm.clicked = !vm.clicked;
         }
