6ebd3666dc3bc9476235ea296549d362f05bcda7
Cam <cameronmcouch@gmail.com>
2019-11-03 19:47:50 -0500

    removed episodes 1-5 due to the program size exceeding 500MB

--- transformers/app/app.config.js
+++ transformers/app/app.config.js
@@ -1,9 +1,6 @@
 angular.module('App').config(['$routeProvider', function($routeProvider){
     $routeProvider
     .when('/', {
-        template: "<comp2></comp2>"
-    })
-    .when('/comp', {
         template: "<comp></comp>"
     })
     .when('/podcast', {
