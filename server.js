34e502942a922bdcf1006b9ba6c5353d16931faa
Cam <cameronmcouch@gmail.com>
2020-04-09 20:47:48 -0400

    fix

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const routes = require('./route');
-require('dotenv').config();
-const bearer = process.env.BEARER;
 
 app.use(express.static("./public"));
 app.use(express.json());
@@ -14,5 +12,3 @@ app.use("/", routes);
 const port = process.env.PORT || 8080; 
 
 app.listen(port, _ => console.log("Server is running on port 8080"));
-
-module.exports = bearer;
