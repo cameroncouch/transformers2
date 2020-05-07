a4c256079ad74b4d2149783e11443bd240b4000d
Cam <cameronmcouch@gmail.com>
2020-05-07 18:01:40 -0400

    updated last build date in rss

--- public/app/app.service2.js
+++ public/app/app.service2.js
@@ -9,7 +9,7 @@ function PricingService($http, $location) {
         method:"GET",
         url: `https://api.tcgplayer.com/v1.32.0/catalog/products?categoryId=57&productName=${query}`,
         headers: {
-          'Authorization': 'Bearer gerjIwcVUNhm5ZBhTAL7gzKMcPkpBqvXeGEi1vpt1T5n29YBY5swE0ZfWwaOFxMlQ1huXlKVE8uBpf4DRXGzjlDEHxqttbPKFigVHAYZ2y-359UgclsmTmki1itHuLK6joabqPfT2nAlZ4INAxDRZXnVBVybtlcINSQavIolkD2k-E7AhgjgSqw1XjZZ7ORfR1tqWzR7sHiLjt3ZLxJM3pHukUGBGB337-wbgYvC1f60gwPyZWD8-b7DsGk7C1GYQsEcfrvEibaCozZKZSclLEDtYmot9Z6K89VUsGTLSTEhHW12xD7IiAoChXFjCssDCd4EOg'
+          'Authorization': 'Bearer oc-GMWUAdIaCdeK-WUkRDmISwKS0o8PYF_TS2uxCxWZbK_O9U6OcuBp_6gqp8faMqSGsDyEviQCpPUvTD8qCApeHZ1DEV-Hct-TWrCd_kJPW8C4c0nYDQkDb34ybYDP_foadWcrp6cdMdeNbf88omwlWbe9eBYXLYRNe5y4ZxQsSwzu5P0cFLbL_Vl75BTu-IQ23ymIbZpDSNqsHddTTEg6bTa5fL34iVZXugB-Pj3DA26JqrDiwoCegJWnnjR4VFe0Kga2v0r00QZzFZkawLvRAUkeIVmqSqmhClg1JFsgcLqbM62A1wmX-Ye3cYYDqpKIy1A'
         },
       }).then((response) => {
         self.card = response.data.results[0];
@@ -22,7 +22,7 @@ function PricingService($http, $location) {
         method:"GET",
         url: `https://api.tcgplayer.com/v1.32.0/pricing/product/${sku}`,
         headers: {
-          'Authorization': 'Bearer 5pKxy3aMz3D9C4Z9ONFue_JP-a2lpFTpDMNf0scGqWKz_vZRihzUcAKw7B5qM9Nky12VhgqSuFX0uObTbWyR-yYvlAM9XYizKsmUvTKkVBCN63y3Vutpc-YomjyzPqpvZtniIIn8YbyQuQY9sc7z1CTCECXG7Zvro4ozmvkvOGsFIIx9IV1r-i5rKC99yeI4gIryZYUMgvZhHqFKZXnLQGgi7NFKHNpJY8y0lZ8JzulprOHgkTNIJVzh5Odnm3ItLE_TWp2at_bKr95r0Eow0M3A-YMJ-aMQx3ix_7YHi5yCOhSvh3LqMTlEWv3iMIJqt8YIoQ'
+          'Authorization': 'Bearer oc-GMWUAdIaCdeK-WUkRDmISwKS0o8PYF_TS2uxCxWZbK_O9U6OcuBp_6gqp8faMqSGsDyEviQCpPUvTD8qCApeHZ1DEV-Hct-TWrCd_kJPW8C4c0nYDQkDb34ybYDP_foadWcrp6cdMdeNbf88omwlWbe9eBYXLYRNe5y4ZxQsSwzu5P0cFLbL_Vl75BTu-IQ23ymIbZpDSNqsHddTTEg6bTa5fL34iVZXugB-Pj3DA26JqrDiwoCegJWnnjR4VFe0Kga2v0r00QZzFZkawLvRAUkeIVmqSqmhClg1JFsgcLqbM62A1wmX-Ye3cYYDqpKIy1A'
         },
       }).then((response) => {
         self.pricing = response.data.results[0];
