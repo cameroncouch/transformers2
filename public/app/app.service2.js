2ca796b2f06f12c87b3214f1a86f725b17782405
Cam <cameronmcouch@gmail.com>
2020-07-12 19:53:51 -0400

    testing serverside api call

--- public/app/app.service2.js
+++ public/app/app.service2.js
@@ -7,10 +7,7 @@ function PricingService($http, $location) {
       console.log(query);
       return $http({
         method:"GET",
-        url: `https://api.tcgplayer.com/v1.32.0/catalog/products?categoryId=57&productName=${query}`,
-        headers: {
-          'Authorization': 'Bearer Fus-pqShHuHrxwFKIxdLEVMzfhpdqEoNb6LOSe8ktjMyP91oa_-gNEzCCRKqgLwDSHxrElUusiYFRDNQt0oOhrr3MYONUZz5EJGn6DzMsY2uklUPfiuSWC_kcdDDzUS8gGMzDbRlObsTeW9n4csTnOc8uaQGbM1KH1OKaufUeTiDwTCwsUrDb3Pu0-dBKdQxAi82HgX4aP_Mml7VlRIHQHYyg7VV1mtx-GIXe-I6atVPsVcjGNichFDW98l7MOs0sZuS5HtUvu2Ly-yvVqAnnwNMOAFqIc5CWLAV43LSQeFI1CNe5K7Qj8Unj-q8FDaANCS_gw'
-        },
+        url: `/cardName/${query}`,
       }).then((response) => {
         self.card = response.data.results[0];
         return self.card;
@@ -20,10 +17,7 @@ function PricingService($http, $location) {
     self.getPrice = (sku) => {
       return $http({
         method:"GET",
-        url: `https://api.tcgplayer.com/v1.32.0/pricing/product/${sku}`,
-        headers: {
-          'Authorization': 'Bearer Fus-pqShHuHrxwFKIxdLEVMzfhpdqEoNb6LOSe8ktjMyP91oa_-gNEzCCRKqgLwDSHxrElUusiYFRDNQt0oOhrr3MYONUZz5EJGn6DzMsY2uklUPfiuSWC_kcdDDzUS8gGMzDbRlObsTeW9n4csTnOc8uaQGbM1KH1OKaufUeTiDwTCwsUrDb3Pu0-dBKdQxAi82HgX4aP_Mml7VlRIHQHYyg7VV1mtx-GIXe-I6atVPsVcjGNichFDW98l7MOs0sZuS5HtUvu2Ly-yvVqAnnwNMOAFqIc5CWLAV43LSQeFI1CNe5K7Qj8Unj-q8FDaANCS_gw'
-        },
+        url: `/cardPrice/${sku}`,
       }).then((response) => {
         self.pricing = response.data.results[0];
         return self.pricing;
--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 const express = require('express');
 const app = express();
 const routes = require('./route');
+const { response } = require('express');
+const https = require('https');
+require('dotenv').config();
 
 app.use(express.static("./public"));
 app.use(express.json());
 app.use("/", routes);
 
-
-
-
 const port = process.env.PORT || 8080; 
 
 app.listen(port, _ => console.log("Server is running on port 8080"));
+
+const bearer = process.env.BEARER
+const cardUrl = `api.tcgplayer.com`
+var cardName;
+var cardSku;
+
+const options1 = {
+    host: cardUrl,
+    path: `/v1.32.0/catalog/products?categoryId=57&productName=${cardName}`,
+    port: 443,
+    headers: {
+        Authorization: `Bearer ${bearer}`
+    }
+}
+const options2 = {
+    host: cardUrl,
+    path: `/v1.32.0/pricing/product/${cardSku}`,
+    port: 443,
+    headers: {
+        Authorization: `Bearer ${bearer}`
+    }
+}
+
+app.get('/cardName/:name', async (req,res) => {
+    cardName = req.params.name;
+    https.get(options1, (response) => {
+        response.on('data', () => {
+            console.log(response);
+        });
+        response.on('end', () => {
+            console.log(response);
+        });
+    }).on('error', (error) => {
+        console.log(error);
+    });
+});
+
+app.get('/cardPrice/:sku', async (req,res) => {
+    cardSku = req.params.sku;
+    https.get(options2, (response) => {
+        response.on('data', () => {
+            console.log(response);
+        });
+        response.on('end', () => {
+            console.log(response);
+        });
+    }).on('error', (error) => {
+        console.log(error);
+    });
+});
+
+module.exports = bearer;
