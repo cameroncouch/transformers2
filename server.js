d5e99501017aa5d19a18cd4f64259672a9529535
Cam <cameronmcouch@gmail.com>
2021-12-21 07:49:34 -0500

    updated server.js

--- server.js
+++ server.js
@@ -7,17 +7,17 @@ app.use(express.static("./public"));
 app.use(express.json());
 app.use("/", routes);
 
-const port = process.env.PORT || 8080; 
+const port = process.env.PORT || 3000; 
 
-app.listen(port, _ => console.log("Server is running on port 8080"));
+app.listen(port, _ => console.log("Server is running on port 3000"));
 
 const bearer = process.env.BEARER
 var cardName;
 var cardSku;
 
-app.get('/cardName/:name', (req,res) => {
+app.get('/cardName/:name', async (req,res) => {
     cardName = req.params.name;
-    return axios.get(`https://api.tcgplayer.com/v1.32.0/catalog/products?categoryId=57&productName=${cardName}`, {
+    return await axios.get(`https://api.tcgplayer.com/v1.37.0/catalog/products?categoryId=57&productName=${cardName}`, {
         headers: {'Authorization': `Bearer ${bearer}`},
         }).then((response) => {
             res.send(response.data);
@@ -29,7 +29,7 @@ app.get('/cardName/:name', (req,res) => {
 
 app.get('/cardPrice/:sku', async (req,res) => {
     cardSku = req.params.sku;
-     return await axios.get(`https://api.tcgplayer.com/v1.32.0/pricing/product/${cardSku}`, {
+     return await axios.get(`https://api.tcgplayer.com/v1.37.0/pricing/product/${cardSku}`, {
         headers: {'Authorization': `Bearer ${bearer}`},
     }).then((response) => {
         res.send(response.data);
